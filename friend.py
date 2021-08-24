efdd0f8f81bd9812be178cc645d88361a10c8c53
Rharmon32 <88513773+Rharmon32@users.noreply.github.com>
2021-08-24 11:20:44 -0500

    Add files via upload

---
+++ friend.py
@@ -0,0 +1,7 @@
+friend = 'Brian'
+cash = 17.50
+
+name = input('What is your friends name? ')
+
+print('Hi Josh, we are getting ready to start, ' + name + ' it is nice to meet you')
+print('how much cash do you have? $' + str(cash))
